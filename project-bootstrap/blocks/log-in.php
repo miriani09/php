ee455e3f8962abecf0848c5a38726a096e7562ee
miriani09 <miriani0996@gmail.com>
2021-05-29 19:33:07 +0400

    php

--- project-bootstrap/blocks/log-in.php
+++ project-bootstrap/blocks/log-in.php
@@ -18,10 +18,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <title>E-Book</title>
+
 </head>
 <body style="background-color: rgb(32, 39, 46) !important;">
 <?php
